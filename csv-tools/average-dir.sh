5c0c1309e304292836eb2032496981595e57623d
Sean Dawson <sean.dawson@noxharmonium.com>
2012-09-18 11:22:58 +1000

    Updated bash script to work with new csv-collate.py

--- csv-tools/average-dir.sh
+++ csv-tools/average-dir.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-for dir in $1*;
-do
-	echo "Calling csv-average.py on " $dir	
-	./csv-average.py -rw $dir ./`basename $dir`.csv
+#for dir in $1*;
+#./average-dir.sh ~/Study/TestData/1/ 10 > 1.csv
+
+#do
+	#echo "Calling csv-average.py on " $1	
+
+	for i in {0..99}
+	do
+		#echo "Files:"	   
+		{ echo -n $i", " && find $1 | grep "\."$i"\.json" | head -n $2 | xargs ./json-dump.py "population>.Critter[Fitness.Changes,Fitness.TotalJourneyMinutes,Rank]" | ./csv-collate.py -n 1 -a 6 -m 6,9 -M 6 -y 100,11,6,9; }
+#	done
+	
+	
 
 
 done
