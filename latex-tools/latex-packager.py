d5104c0bf526ceb3a6954d2a24966f2a7729c901
Sean Dawson <sean.dawson@noxharmonium.com>
2013-01-08 23:19:32 +1100

    Program is now fully functioning. Next step is testing.

--- latex-tools/latex-packager.py
+++ latex-tools/latex-packager.py
@@ -9,6 +9,8 @@ import glob
 import os
 import sys
 import time
+import shutil
+import tempfile
 from subprocess import call
 
 # Constants
@@ -42,14 +44,14 @@ parser.add_argument('-f', action="store_true",default=False,
 		help='force run. Skips error messages.')
 
 group = parser.add_mutually_exclusive_group()
-group.add_argument('-z', action="store_true",default=False, 			
+group.add_argument('-z', action="store_true",default=True, 			
 		help='Use tar/gzip compression. (Default)')
 group.add_argument('-j', action="store_true",default=False, 			
 		help='Use tar/bzip2 compression.')
-group.add_argument('-7', action="store_true",default=False, 			
+group.add_argument('-s', action="store_true",default=False, 			
 		help='Use 7z compression. (Requires p7zip)')
 group.add_argument('-p', action="store_true",default=False, 			
-		help='Use zip compression.v (Requires zip)')
+		help='Use zip compression. (Requires zip)')
 
 parser.add_argument('--latex', default='pdflatex', 
 		help='specify the latex complier to use when generating .fls files. (Default: pdflatex)')
@@ -60,6 +62,9 @@ parser.add_argument('tex_file', type=str, nargs='+',
 parser.add_argument('destination', type=str, 
 		help='the output file name')
 
+
+# Save the current directory to restore it
+currentDir = os.path.dirname(os.path.abspath(__file__))
 
 # Parse the given args, show help on invalid args.
 args = parser.parse_args()
@@ -72,6 +77,7 @@ for filename in args.tex_file:
 	extension = fileSplit[-1]
 	rawfilename = "".join(fileSplit[0:-1])
 	flsfilename = rawfilename + '.fls'
+
 
 	debug("filename: '{0}'".format(filename))
 	debug("flsfilename: '{0}'".format(flsfilename))
@@ -99,7 +105,8 @@ for filename in args.tex_file:
 		if (flsMTime < texMTime):
 			print("The .fls file has a modification date that is earlier than the tex file. It will need to be regenerated.")
 			createfls = True
-
+		else:
+			debug("The .fls file is up to date.")
 	
 	# Regenerate fls files if needed
 	if (createfls):
@@ -112,17 +119,72 @@ for filename in args.tex_file:
 		# Blank answer defaults to yes
 		answer = None
 		while (not(answer == 'y' or answer == 'n' or answer == '')):
-			answer = input().lower().strip()
+			answer = raw_input().lower().strip()
 			
 		if (answer == '' or answer == 'y'):
 			command = "{0} -recorder {1}".format(args.latex,rawfilename)
 			print ("Executing: " + command )
-			call(command.split(' '))
+			error_code = call(command.split(' '), cwd=os.path.dirname(filename))
+			if (error_code != 0):
+				print ("Warning: Latex complile returned errorcode {0}.".format(error_code))
+				exit_warning()
 		else:
 			print ("Warning: You cannot proceed without generating a valid .fls file.") 
 			exit_warning()
 
+	# By this point the fls file should be valid
+	# Read in file	
+	debug("Reading in fls file...")
+	flsFile = open(flsfilename, 'r')
+	flsLines = flsFile.readlines()
+	
+	debug("Creating temp directory...")
+	tempDir = tempfile.mkdtemp()
+	debug("Temp directory is at '{0}'".format(tempDir))
 
+	for line in flsLines:
+		splitLine = line.split(' ')
+		commandCode = splitLine[0]
+		if (commandCode == 'INPUT'):
+			inputFile = splitLine[1].strip()
+			targetFile = tempDir + '/' + os.path.basename(inputFile)
+			debug("Copy: {0} -> {1}".format(inputFile,targetFile))
+			os.chdir(os.path.dirname(filename))
+			
+			shutil.copyfile(inputFile, targetFile)
+			os.chdir(currentDir)
+		
+	error_code = 0;	
+		
+
+	debug("Starting compression operation...")
+	arc_format = '';
+
+	if (args.z):
+		arc_format = 'gztar'
+		
+	if (args.j):
+		arc_format = 'bztar'
+
+	if (args.p):
+		arc_format = 'zip'
+
+	if (args.s):
+		error_code = call(['7zr','a',args.destination,'.'], cwd=os.path.dirname(filename))
+	else:
+		shutil.make_archive(
+			os.path.abspath(args.destination),
+			arc_format,
+			tempDir,
+			verbose=args.v)
+
+
+	if (error_code != 0):
+		print ("Warning: Latex complile returned errorcode {0}.".format(error_code))
+		exit_warning()
+	
+	debug("Removing temp directory...")
+	shutil.rmtree(tempDir)
 		
 		
 
